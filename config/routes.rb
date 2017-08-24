09f9dbd1447df2b31a22ac5de2ab1d597ffe8e49
konradrog <konrad1210@gmail.com>
2017-08-24 09:26:43 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  root "welcome#index" #tutaj zmieniamy stronę główną
+
+  resources :articles
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
