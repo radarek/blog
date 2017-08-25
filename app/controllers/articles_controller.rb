720dda60c26f81c7168a95a79addb9dbcaaa167c
Radosław Bułat <radek.bulat@gmail.com>
2017-08-25 09:21:47 +0200

    Add articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,47 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
   end
 
   def create
-    render plain: params[:article]
+    article_params = params.require(:article).permit(:title, :text)
+    @article = Article.create(article_params)
+
+    if @article.new_record?
+      render "new"
+    else
+      redirect_to article_path(@article)
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all.order(id: :desc)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    article_params = params.require(:article).permit(:title, :text)
+
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+
+    redirect_to articles_path
   end
 end
