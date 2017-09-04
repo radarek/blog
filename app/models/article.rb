de70876a5beb0d0b2e31be9d5efff8c0975441a0
Radosław Bułat <radek.bulat@gmail.com>
2017-09-04 12:58:59 +0200

    More relations and scopes

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,10 @@ class Article < ApplicationRecord
 
   has_many :comments, dependent: :destroy
   has_many :commenters_ips, through: :comments, source: "ip"
+  has_and_belongs_to_many :categories
+
+  scope :younger_than_week, -> { where("created_at > ?", 1.week.ago) }
+  scope :with_super_titles, -> { where("title like ?", "%super%") }
 
   def next_article
     Article.where("id > ?", id).order(:id).first
