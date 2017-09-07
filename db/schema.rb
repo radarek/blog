4db3c668f4532758a495ce5f0e38c1bb0858fbca
Radosław Bułat <radek.bulat@gmail.com>
2017-09-07 14:56:51 +0200

    Add users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170904121659) do
+ActiveRecord::Schema.define(version: 20170905100116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,13 @@ ActiveRecord::Schema.define(version: 20170904121659) do
 
   create_table "ips", force: :cascade do |t|
     t.string "address", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
